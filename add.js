18f5e528530b633cf9b9d1a8c0dec208e118a978
dlckdrjs <cklee0518@naver.com>
2022-10-17 18:54:46 +0900

    1.5 Drawing Project Two

--- add.js
+++ add.js
@@ -6,12 +6,14 @@ canvas.height = 800;
 
 
 
-ctx.fillRect(200, 200, 50, 200)
-ctx.fillRect(400, 200, 50, 200)
-ctx.lineWidth = 4;
-ctx.strokeRect(300, 300, 50, 100)
-ctx.fillRect(200, 200, 200, 20)
-ctx.moveTo(200, 200)
-ctx.lineTo(325, 100)
-ctx.lineTo(450, 200)
+ctx.fillRect(210, 200, 15, 100)
+ctx.fillRect(350, 200, 15, 100)
+ctx.fillRect(260, 200, 60, 200)
+ctx.arc(250, 100, 50, 0, 2 * Math.PI)
+ctx.fill()
+
+ctx.beginPath()
+ctx.arc(260, 80, 10, Math.PI, 2 * Math.PI)
+ctx.arc(220, 80, 10, Math.PI, 2 * Math.PI)
+ctx.fillStyle = "blue"
 ctx.fill()
