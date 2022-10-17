b6e1dbc9cbc7ebf5c0232adc14147999d679d188
dlckdrjs <cklee0518@naver.com>
2022-10-17 19:45:15 +0900

    2.0 Painting Lines

--- add.js
+++ add.js
@@ -4,16 +4,29 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
+const colors = [
+    "#00a8ff",
+    "#9c88ff",
+    "#f1c40f",
+    "#95a5a6",
+    "#2ecc71",
+    "#e84118",
+    "#e67e22",
+    "#bdc3c7",
+    "#f39c12"
+
+]
+
+ctx.lineWidth = 2
 
 
-ctx.fillRect(210, 200, 15, 100)
-ctx.fillRect(350, 200, 15, 100)
-ctx.fillRect(260, 200, 60, 200)
-ctx.arc(250, 100, 50, 0, 2 * Math.PI)
-ctx.fill()
+function onClick(event) {
+    ctx.beginPath()
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    ctx.strokeStyle = color
+    ctx.moveTo(0, 0,)
+    ctx.lineTo(event.offsetX, event.offsetY)
+    ctx.stroke()
+}
 
-ctx.beginPath()
-ctx.arc(260, 80, 10, Math.PI, 2 * Math.PI)
-ctx.arc(220, 80, 10, Math.PI, 2 * Math.PI)
-ctx.fillStyle = "blue"
-ctx.fill()
+canvas.addEventListener("mousemove", onClick)
