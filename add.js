7dc0a78c6ad02665bb56eb669f157d8511d893ca
dlckdrjs <cklee0518@naver.com>
2022-10-18 23:26:18 +0900

    2.1 Mouse Painting

--- add.js
+++ add.js
@@ -1,32 +1,30 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+let isPainting = false
+
 canvas.width = 800;
 canvas.height = 800;
 
-const colors = [
-    "#00a8ff",
-    "#9c88ff",
-    "#f1c40f",
-    "#95a5a6",
-    "#2ecc71",
-    "#e84118",
-    "#e67e22",
-    "#bdc3c7",
-    "#f39c12"
-
-]
-
-ctx.lineWidth = 2
-
-
-function onClick(event) {
-    ctx.beginPath()
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    ctx.strokeStyle = color
-    ctx.moveTo(0, 0,)
-    ctx.lineTo(event.offsetX, event.offsetY)
-    ctx.stroke()
+function onMove(event) {
+    if (isPainting == true) {
+        ctx.lineTo(event.offsetX, event.offsetY)
+        ctx.stroke()
+        return
+    }
+    ctx.moveTo(event.offsetX, event.offsetY)
 }
 
-canvas.addEventListener("mousemove", onClick)
+function startPainting() {
+    isPainting = true
+}
+
+function cancelPainting() {
+    isPainting = false
+}
+
+
+canvas.addEventListener("mousemove", onMove)
+canvas.addEventListener("mousedown", startPainting)
+canvas.addEventListener("mouseup", cancelPainting)
+canvas.addEventListener("mouseleave", cancelPainting)
