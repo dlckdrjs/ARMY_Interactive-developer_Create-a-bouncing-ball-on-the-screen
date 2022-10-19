3da16753b96aae436c6cd4827c1ac06fdba3f2d8
dlckdrjs <cklee0518@naver.com>
2022-10-19 22:22:47 +0900

    self-directed-learning(reset, fill)
    버튼을 눌렀을때 그림이 다 사라지는 reset기능과, 원하는 색갈에 화면 한번만 click을 하면 화면 전체 색 채우는 기능을 만들었다.

--- add.js
+++ add.js
@@ -3,9 +3,14 @@ const ctx = canvas.getContext("2d")
 
 const lineWidth = document.getElementById("line-width")
 
+const modeBtn = document.getElementById("mode-btn-fill")
+
+
 const eraser = document.getElementById("eraser-btn")
 const color = document.getElementById("color")
 const colorOption = Array.from(document.getElementsByClassName("color-option"))
+const reset = document.getElementById("reset-btn")
+
 
 canvas.width = 800
 canvas.height = 800
@@ -13,6 +18,7 @@ canvas.height = 800
 ctx.lineWidth = lineWidth.value
 
 let isPainting = false
+let isFilling = false
 
 function onMouseMove(event) {
     if (isPainting) {
@@ -37,28 +43,63 @@ function onLineWidthChange(event) {
 
 }
 
-function onEraserClick() {
-    ctx.strokeStyle = "white"
-}
+
 
 function onColocChange(event) {
     ctx.strokeStyle = event.target.value
+    ctx.fillStyle = event.target.value
+
 }
 
 function onColorOptionClick(event) {
     ctx.strokeStyle = event.target.dataset.color
+    ctx.fillStyle = event.target.dataset.color
     color.value = event.target.dataset.color
 
+}
+
+
+
+function modeBtnClick() {
+    if (isFilling) {
+        isFilling = false
+        modeBtn.innerText = "Fill"
+    } else {
+        isFilling = true
+        modeBtn.innerText = "Draw"
+    }
+
+}
+
+function onCavasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, 800, 800)
+    }
+
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = "white"
+    modeBtn.innerText = "Draw"
+}
+
+function onResetClick() {
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, 800, 800)
 }
 
 canvas.addEventListener("mousemove", onMouseMove)
 canvas.addEventListener("mousedown", startPainting)
 canvas.addEventListener("mouseup", cancelPainting)
 canvas.addEventListener("mouseleave", cancelPainting)
+canvas.addEventListener("click", onCavasClick)
 
 lineWidth.addEventListener("change", onLineWidthChange)
 
 eraser.addEventListener("click", onEraserClick)
+reset.addEventListener("click", onResetClick)
 
 color.addEventListener("change", onColocChange)
 colorOption.forEach(color => color.addEventListener("click", onColorOptionClick))
+
+modeBtn.addEventListener("click", modeBtnClick)
