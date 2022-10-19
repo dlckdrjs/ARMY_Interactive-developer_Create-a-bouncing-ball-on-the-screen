eb63c23d6c2f89207c7d95d088c1b750ca5415a1
dlckdrjs <cklee0518@naver.com>
2022-10-19 14:43:41 +0900

    2.5 Filling Mode

--- add.js
+++ add.js
@@ -3,11 +3,12 @@ const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width")
 const color = document.getElementById("color")
 const colorOption = Array.from(document.getElementsByClassName("color-option"))
+const modeBtn = document.getElementById("mode-btn")
 
-
-
+console.log(modeBtn)
 
 let isPainting = false
+let isFilling = false
 
 canvas.width = 800;
 canvas.height = 800;
@@ -17,7 +18,7 @@ ctx.lineWidth = lineWidth.value
 
 
 function onMove(event) {
-    if (isPainting == true) {
+    if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY)
         ctx.stroke()
         return
@@ -53,12 +54,30 @@ function onColorClick(event) {
 
 }
 
+function onModeBtn() {
+    if (isFilling) {
+        isFilling = false
+        modeBtn.innerText = "Fill"
+    } else {
+        isFilling = true
+        modeBtn.innerText = "Draw"
+    }
+}
+
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, 800, 800)
+    }
+}
+
 
 
 canvas.addEventListener("mousemove", onMove)
 canvas.addEventListener("mousedown", startPainting)
 canvas.addEventListener("mouseup", cancelPainting)
 canvas.addEventListener("mouseleave", cancelPainting)
+canvas.addEventListener("click", onCanvasClick)
 lineWidth.addEventListener("change", onLineWidthChange)
 color.addEventListener("change", onColorChange)
 colorOption.forEach(color => color.addEventListener("click", onColorClick))
+modeBtn.addEventListener("click", onModeBtn)
